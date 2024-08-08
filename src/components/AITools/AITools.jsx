b6a604db51dcc4c30a2b4a5ca869178293f15068
Eliza Pradhan <pradhaneliza2@gmail.com>
2024-08-08 16:31:12 -0700

    Add button component.

--- src/components/AITools/AITools.jsx
+++ src/components/AITools/AITools.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import Button from "../Button/Button";
 
 export const AITools = () => {
-  return <div>AITools</div>;
+  return <div>AITools
+    <Button text="Sign Up" />
+  </div>;
 };
