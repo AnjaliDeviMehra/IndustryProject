6f791cee08d5d3965231ab881bb23da9477b2aa9
mickey-vee <mvoong27@gmail.com>
2024-08-08 21:34:35 -0400

    Added links and fixed cards

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -108,15 +108,16 @@ export const HomePage = () => {
         <div className="spotlight__wrapper">
           <div className="spotlight__card">
             <div>
-              <img
-                src="../../../src/assets/images/plan.svg"
-                alt=""
-                className="spotlight__icon"
-              />{" "}
-              <div>
-                {" "}
-                <span>Plan</span>
-                <span>Learn the basics of Azure: AI Fundamentals</span>
+              <div className="spotlight__description">
+                <img
+                  src="../../../src/assets/images/plan.svg"
+                  alt=""
+                  className="spotlight__icon"
+                />
+                <div>
+                  <h3>Plan</h3>
+                  <span>Learn the basics of Azure: AI Fundamentals</span>
+                </div>
               </div>
               <div>
                 <p>
@@ -125,45 +126,61 @@ export const HomePage = () => {
                   conversational AI in this curated set of content and prepare
                   for Exam AI-900."
                 </p>
-              </div>
-            </div>
-          </div>{" "}
-          <div className="spotlight__card">
-            <div>
-              <img
-                src="../../../src/assets/images/azure-openai-icon.svg"
-                alt=""
-                className="spotlight__icon"
-              />
-              <span>Plan</span>
-              <span>Learn the basics of Azure: AI Fundamentals</span>
-              <div>
-                <p>
-                  "Learn about many areas of AI, including machine learning,
-                  computer vision, natural language processing, and
-                  conversational AI in this curated set of content and prepare
-                  for Exam AI-900."
-                </p>
+                <a href="" className="spotlight__link">
+                  Get started
+                </a>
               </div>
             </div>
           </div>
+
           <div className="spotlight__card">
             <div>
-              {" "}
-              <img
-                src="../../../src/assets/images/copilot-icon.png"
-                alt=""
-                className="spotlight__icon"
-              />
-              <span>Plan</span>
-              <span>Learn the basics of Azure: AI Fundamentals</span>
+              <div className="spotlight__description">
+                <img
+                  src="../../../src/assets/images/azure-openai-icon.svg"
+                  alt=""
+                  className="spotlight__icon"
+                />
+                <div>
+                  <h3>AI Empowerment Hub</h3>
+                  <span>Explore AI Use Cases</span>
+                </div>
+              </div>
               <div>
                 <p>
-                  "Learn about many areas of AI, including machine learning,
-                  computer vision, natural language processing, and
-                  conversational AI in this curated set of content and prepare
-                  for Exam AI-900."
+                  Discover tailored AI solutions for your industry. Learn how to
+                  integrate AI into your business processes with easy-to-follow
+                  guides and success stories.
                 </p>
+                <a href="" className="spotlight__link">
+                  Get started
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className="spotlight__card">
+            <div>
+              <div className="spotlight__description">
+                <img
+                  src="../../../src/assets/images/copilot-icon.png"
+                  alt=""
+                  className="spotlight__icon"
+                />
+                <div>
+                  <h3>Copilot learning hub</h3>
+                  <span>Learn to use Copilot</span>
+                </div>
+              </div>
+              <div>
+                <p>
+                  "Explore curated content on Microsoft Copilot for technical
+                  professionals to leverage AI technology and enhance
+                  productivity."
+                </p>
+                <a href="" className="spotlight__link">
+                  Start your Copilot learning journey
+                </a>
               </div>
             </div>
           </div>
